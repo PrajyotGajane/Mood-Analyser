995ced5749800900d8ccd7a876bd15dd86bedfbc
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-20 00:46:29 +0530

    Added:Test for Sad and Happy

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -7,13 +7,13 @@ public class MoodAnalyserTest {
     @Test
     public void givenMoodSad_ShouldReturn_Sad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is a Sad message");
+        String mood = moodAnalyser.analyseMood("I am in Sad Mood");
         Assert.assertEquals("Sad",mood);
     }
     @Test
-    public void givenMoodHappy_ShoudldReturn_Happy() {
+    public void givenMoodHappy_ShouldReturn_Happy() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is a Happy message");
+        String mood = moodAnalyser.analyseMood("I am in Any Mood");
         Assert.assertEquals("Happy",mood);
     }
 }
