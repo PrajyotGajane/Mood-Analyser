1e74c6556a171db7892d8a79fc6af33aece1fc75
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-20 00:35:40 +0530

    Added: Test cases for mood analysis

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,4 +1,12 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
+    public String analyseMood(String message){
+    if (message.contains("Sad")) {
+            return "Sad";
+        }
+    else{
+            return "Happy";
+        }
+    }
 }
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -1,10 +1,19 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
     @Test
-    public void testAnalysisMethod() {
+    public void givenMoodSad_ShouldReturn_Sad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a Sad message");
+        Assert.assertEquals("Sad",mood);
+    }
+    @Test
+    public void givenMoodHappy_ShoudldReturn_Happy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a Happy message");
+        Assert.assertEquals("Happy",mood);
     }
 }
