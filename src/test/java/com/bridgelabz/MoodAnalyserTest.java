770e75965e5b4bc41fe687238c38b7689ffecb63
PrajyotGajane <prajyotgajane@gmail.com>
2020-06-19 23:50:58 +0530

    Added:MoodAnalyser class

---
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+    @Test
+    public void testAnalysisMethod() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+    }
+}
